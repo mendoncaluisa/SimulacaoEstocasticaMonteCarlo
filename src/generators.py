f31d0a3d00930d6c03e2070905903ee57d94f0a0
Poliana <polianasamonte@gmail.com>
2025-10-30 20:56:47 -0300

    distribuição bernoulli funcionando

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,14 @@
+#region Distribuição Bernoulli
 import random
+import matplotlib.pyplot as plt
 
 ## Gerador de Números Aleatórios Segundo Distribuição de Bernoulli
 def bernoulli_generator(p):
     # p é a taxa de sucesso
     random_number = random.random() # gerando um número aleatório entre 0 e 1
-    if random_number <= p:
+    if random_number < p:
         return 1
     else:
         return 0
+#endregion
+
