e90a4c0b58a1d3bd6da5d964a2db8815c491c1ae
Victor Luis <victor.rbread@gmail.com>
2025-11-03 22:35:19 -0300

    implementação do pseudocodigo da fase 2

--- src/model.py
+++ src/model.py
@@ -10,3 +10,49 @@ def terreno_laje_acabamento_interno (pert_min, per_moda, pert_max, log_normal_me
     custo_total = custo_mao_obra + custo_material
 
     return duracao_total, custo_total
+
+
+def fundacao(fase):
+    # Decide qual empresa vai fzr o trabalho
+    empresa_A_escolhida = bernoulli(fase['p_empresa_A'])
+    
+    # Processo normal
+    if empresa_A_escolhida:
+        tempo_fundacao = pert(
+            fase['empresa_A']['pert'][0], 
+            fase['empresa_A']['pert'][1], 
+            fase['empresa_A']['pert'][2])
+        
+        custo_material = log_normal(
+            fase['empresa_A']['log_normal'][0], 
+            fase['empresa_A']['log_normal'][1])
+        
+        custo_mao_obra = normal(
+            fase['empresa_A']['normal'][0], 
+            fase['empresa_A']['normal'][1])
+        
+    else:
+        tempo_fundacao = pert(
+            fase['empresa_B']['pert'][0], 
+            fase['empresa_B']['pert'][1], 
+            fase['empresa_B']['pert'][2])
+        
+        custo_material = log_normal(
+            fase['empresa_B']['log_normal'][0], 
+            fase['empresa_B']['log_normal'][1])
+        
+        custo_mao_obra = normal(
+            fase['empresa_B']['normal'][0], 
+            fase['empresa_B']['normal'][1])
+
+    # Decide se ocorreu algum evento geologico
+    evento_geologico_ocorreu = bernoulli(fase['p_evento_geologico'])
+
+    if evento_geologico_ocorreu:
+        tempo_fundacao += fase['tempo_extra']
+        custo_fundacao = custo_mao_obra + custo_material + fase['custo_extra']
+    else:
+        custo_fundacao = custo_mao_obra + custo_material
+
+    return tempo_fundacao, custo_fundacao
+
