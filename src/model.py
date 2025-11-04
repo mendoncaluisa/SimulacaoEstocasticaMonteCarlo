ab3747c6d829b90417f3a0119798f0469e9fb638
Poliana <polianasamonte@gmail.com>
2025-11-04 12:52:55 -0300

    iniciando a fase de alvenaria

--- src/model.py
+++ src/model.py
@@ -1,8 +1,8 @@
 from src.generators import pert, normal
 
 
-def terreno_laje_acabamento_interno (pert_min, per_moda, pert_max, log_normal_media, log_normal_desvio, normal_media, normal_desvio):
-    duracao = pert(pert_min, per_moda, pert_max)
+def terreno_laje_acabamento_interno (pert_min, pert_moda, pert_max, log_normal_media, log_normal_desvio, normal_media, normal_desvio):
+    duracao = pert(pert_min, pert_moda, pert_max)
     custo_material = log_normal()
     custo_mao_obra = normal(normal_media, normal_desvio)
 
@@ -10,3 +10,12 @@ def terreno_laje_acabamento_interno (pert_min, per_moda, pert_max, log_normal_me
     custo_total = custo_mao_obra + custo_material
 
     return duracao_total, custo_total
+
+
+#region Fase de Alvenaria
+def fase_de_alvenaria(pert_min, pert_moda, pert_max, log_normal_media, log_normal_desvio,normal_media, normal_desvio, necessidade_retrabalho, custo_retrabalho, tempo_retrabalho):
+    pass
+    #duracao_total = normal()
+#endregion
+
+
