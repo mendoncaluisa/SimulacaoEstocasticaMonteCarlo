6af8051bee6e35639234e33eed1c4e6637095c00
Maria Luísa Mendonca <marialuisa.computacao@gmail.com>
2025-11-05 20:26:14 -0300

    esboço da pintura externa

--- src/model.py
+++ src/model.py
@@ -1,4 +1,4 @@
-from src.generators import pert, normal
+from src.generators import pert, normal, bernoulli
 
 
 def terreno_laje_acabamento_interno (pert_min, per_moda, pert_max, log_normal_media, log_normal_desvio, normal_media, normal_desvio):
@@ -9,4 +9,30 @@ def terreno_laje_acabamento_interno (pert_min, per_moda, pert_max, log_normal_me
     duracao_total = duracao
     custo_total = custo_mao_obra + custo_material
 
+    return duracao_total, custo_total
+
+def pintura_externa(taxa_empresa_tercerizada, taxa_condicao_climatica,
+                    empresa_a_log_normal, empresa_a_normal, empresa_a_pert_tempo_bom, empresa_a_pert_tempo_ruim,
+                    empresa_b_log_normal, empresa_b_normal, empresa_b_pert_tempo_bom, empresa_b_pert_tempo_ruim):
+    escolha_tercerizada = bernoulli(taxa_empresa_tercerizada)
+    condicao_climatica = bernoulli(taxa_condicao_climatica)
+
+    if escolha_tercerizada == 1:
+        custo_material = log_normal(empresa_a_log_normal)
+        mao_obra = normal(empresa_a_normal)
+        if condicao_climatica == 1:
+            duracao = pert(empresa_a_pert_tempo_ruim)
+        else:
+            duracao = pert(empresa_a_pert_tempo_bom)
+
+    else:
+        custo_material = normal(empresa_b_log_normal)
+        mao_obra = normal(empresa_b_normal)
+        if condicao_climatica == 1:
+            duracao = pert(empresa_b_pert_tempo_ruim)
+        else:
+            duracao = pert(empresa_b_pert_tempo_bom)
+
+    duracao_total = duracao
+    custo_total = custo_material + mao_obra
     return duracao_total, custo_total
